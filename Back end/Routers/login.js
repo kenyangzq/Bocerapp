c80c076ccba4144728d2d81a851e86b39daeb946
Vurchin <yicheng.wang@vanderbilt.edu>
2016-06-24 16:07:43 -0700

    back end update

---
+++ Back end/Routers/login.js
@@ -0,0 +1,16 @@
+//Yicheng Wang 06/24/2016
+
+//inporting the needed packages
+var express = require('express');
+var user = require('../dataModel');
+
+
+var router = express.Router;
+
+
+router.get('/LOGIN',function(req,res,err){
+
+})
+
+
+
---
+++ Back end/dataModel/user.js
@@ -0,0 +1,11 @@
+// Yicheng Wang   06/24/2016
+
+//user constructor
+function user(name, pass){
+	this.username = name;
+	this.password = pass;
+}
+
+user.prototype.match = function(){ //class method to check if the given password have matching user
+	
+}
