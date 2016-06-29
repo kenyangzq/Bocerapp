13a2793ef7df7bb4472c7aed38ad7ef4743cad39
dempsy95 <dempsy95@hotmail.com>
2016-06-29 14:19:35 +0800

    update

---
+++ Back end/app.js
@@ -0,0 +1,11 @@
+//import needed files
+var express = require('express');
+
+
+var app = express();
+
+
+
+app.listen(8000,function(){ //listen to localhost at port 8000
+	console.log("server listening");
+})
